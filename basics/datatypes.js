498a8d96b1ad0a6dfc57242cd323b6b0e733954c
Vishwesh2294 <136895557+Vishwesh2294@users.noreply.github.com>
2024-05-21 12:56:30 +0000

    types of data type

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+let name = "vishu"
+let age = 20 
+let isLoggedIn = true
+
+
+//number = 2 to the power 53
+//bigInt
+//string = " "
+// boolean = true/false
+// null => standalone value
+// undefined
+//symbol => unique 
+
+//object
+
+console.log(typeof undefined)   //undefined
+console.log(typeof null)         //object
+
+
+
+
+
+
+
